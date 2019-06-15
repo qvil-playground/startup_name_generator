96ed573a4cbe094c1d3d10051e7caf4aad72b47b
현태수 <qvil@op.gg>
2019-06-15 18:04:14 +0900

    Add interactivity

--- lib/main.dart
+++ lib/main.dart
@@ -55,6 +55,11 @@ class RandomWordsState extends State<RandomWords> {
         alreadySaved ? Icons.favorite : Icons.favorite_border,
         color: alreadySaved ? Colors.red : null,
       ),
+      onTap: () {
+        setState(() {
+          alreadySaved ? _saved.remove(pair) : _saved.add(pair);
+        });
+      },
     );
   }
 }
